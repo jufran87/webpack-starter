cf2a2615d016d74693129649047579d6ab9044f3
Julio Cesar Franco <jcfrancoca@unadvirtual.edu.co>
2021-01-23 11:29:42 -0500

    Se corrige ortografia en componente

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -3,6 +3,6 @@ import '../css/componentes.css';
 export const saludar = nombre => {
   console.log('Creando etiqueta h1');
   const h1 = document.createElement('h1');
-  h1.innerText = `Hola, ${nombre} como vas?`;
+  h1.innerText = `Hola ${nombre}, ¿cómo vas?`;
   document.body.append(h1);
 };
